5c08801842146a7ae8a55ce0cc36fc14bab4b9f0
okabi <okabi13@yahoo.co.jp>
2014-10-21 16:08:36 +0900

    形態素分解用のクラスを書いた

---
+++ morpheme.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+require 'MeCab'
+
+## 形態素解析クラス。とりあえず文字列を形態素に分解するだけ。
+class Morpheme
+  ## 与えられた文字列を形態素に分解する
+  def analysis(text)
+    n = @parser.parseToNode(text)
+    result = []
+    while n
+      str = n.surface.to_s
+      result.push(str) if !(str.empty?)
+      n = n.next
+    end
+    return result
+  end
+
+
+  ## コンストラクタ
+  def initialize
+    @parser = MeCab::Tagger.new
+  end
+end
